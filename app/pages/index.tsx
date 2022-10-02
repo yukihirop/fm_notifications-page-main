60c78286230d33883e5a95157ad24c635df2b498
yukihirop <te108186@gmail.com>
2022-10-02 23:45:35 +0900

    fix: 🐛 pages

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -2,9 +2,10 @@ import Layout from '../components/layout'
 import NotificationPage, { Props as NotificationPageProps } from '../components/notification-page'
 import { INotification } from '../interfaces'
 
-const baseURL = process.env.NEXT_PUBLIC_VERCEL_URL ? process.env.NEXT_PUBLIC_VERCEL_URL : 'http://localhost:3000'
-
 export async function getServerSideProps() {
+  const baseURL = process.env.NEXT_PUBLIC_VERCEL_URL
+    ? process.env.NEXT_PUBLIC_VERCEL_URL
+    : "http://localhost:3000";
   const res = await fetch(`${baseURL}/api/notifications`);
   const items: INotification[] = await res.json();
   const unreadCount = items.filter((i) => i.isUnread).length;
